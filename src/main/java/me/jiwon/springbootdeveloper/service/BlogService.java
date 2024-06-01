cae8312760af2bd8bf64933a79235530e4914726
alice0503 <alice010503@gmail.com>
2024-06-01 13:39:54 +0900

    뷰 작성 진행중( 드롭다운, ui 추가, 배경 추가, 영역 정리)

--- src/main/java/me/jiwon/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/jiwon/springbootdeveloper/service/BlogService.java
@@ -2,6 +2,7 @@ package me.jiwon.springbootdeveloper.service;
 
 import lombok.RequiredArgsConstructor;
 import me.jiwon.springbootdeveloper.domain.Article;
+import me.jiwon.springbootdeveloper.dto.AddArticleRequest;
 import me.jiwon.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,9 @@ public class BlogService {
 
     private final BlogRepository blogRepository;
 
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
     public List<Article> findAll() {
         return blogRepository.findAll();
     }
